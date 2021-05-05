916a73c4f8ce41865c2cf21eb2369cb6ae32a321
vrindasaraf90 <vrindasaraf90@gmail.com>
2021-05-04 22:25:10 -1200

    5.2.1 Done!

---
+++ app/src/main/java/com/streamliners/codelab/MainActivity.java
@@ -0,0 +1,33 @@
+package com.streamliners.codelab;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    private int qty = 0;
+    private TextView mShowCount;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mShowCount = (TextView) findViewById(R.id.show_count);
+    }
+
+
+    public void incQty(View view) {
+        ++qty;
+        if (mShowCount != null)
+            mShowCount.setText(Integer.toString(qty));
+    }
+
+    public void showToast(View view) {
+        Toast toast = Toast.makeText(this, R.string.toast_message, Toast.LENGTH_SHORT);
+        toast.show();
+    }
+}
